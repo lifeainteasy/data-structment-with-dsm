f53535e19dce903ae484c206c317184b192865cf
최종근 <zofqofhtltm2@gmail.com>
2020-11-06 23:57:56 +0900

    Create 스택(연결리스트).cpp

---
+++ 스택(연결리스트).cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+typedef struct Node * NodePointer;
+
+typedef struct  Node{
+	int data;
+	NodePointer	link;
+}Node;
+
+
+
+NodePointer top = NULL;
+
+
+void pop(NodePointer preLink){
+		
+} 
+void push(){
+	
+}
+int main(){
+	
+}
