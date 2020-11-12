1cb5fb5fc2942d66d647ec0d772c3543b01414a7
최종근 <zofqofhtltm2@gmail.com>
2020-11-12 17:04:16 +0900

    Update 스택(연결리스트).cpp

--- 스택(연결리스트).cpp
+++ 스택(연결리스트).cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 typedef struct Node * NodePointer;
 
 typedef struct  Node{
@@ -11,12 +12,38 @@ typedef struct  Node{
 NodePointer top = NULL;
 
 
-void pop(NodePointer preLink){
-		
+int pop(){
+	if(!top) return printf("Stack is empty!\n");
+	top = top->link ;
 } 
-void push(){
 	
+
+void push(int value){
+	NodePointer newNode = (NodePointer)malloc(sizeof(Node));
+	newNode->data = value;
+	newNode->link  = top;
+	top=newNode;	
 }
 int main(){
+	push(7);
+	push(6);
+	push(5);
+	push(4);
+	push(3);
+	push(2);
+	push(1);
+	pop();
+	pop();
+	pop();
+	pop();
+	pop();
+	pop();
+	pop();
+	pop();
+	push(1);
+	push(2);
+	for(NodePointer Ptr = top;Ptr;Ptr=Ptr->link ){
+		printf("%d\n",Ptr->data);
+	}
 	
 }
